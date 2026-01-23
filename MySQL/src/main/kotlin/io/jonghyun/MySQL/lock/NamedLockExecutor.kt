843ea8e65650b690c992b0c36b5e2fcd2416210d
jonghyunhub <chuwhdgus@gmail.com>
2026-01-23 21:45:26 +0900

    주석 코드 추가

--- MySQL/src/main/kotlin/io/jonghyun/MySQL/lock/NamedLockExecutor.kt
+++ MySQL/src/main/kotlin/io/jonghyun/MySQL/lock/NamedLockExecutor.kt
@@ -1,6 +1,7 @@
 package io.jonghyun.MySQL.lock
 
 
+import jakarta.transaction.Transactional
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.sql.Connection
@@ -11,7 +12,8 @@ class NamedLockExecutor(
     private val dataSource: DataSource
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
-    
+
+    // @Transactional <- 이 메서드에 트랜잭셔널 추가하면 락 획득/해제 커넥션과 비즈니스 커넥션을 동일하게 고정시켜 버리니 주의!
     fun <T> executeWithLock(lockKey: String, timeout: Int = 10, action: () -> T): T {
         // 커넥션을 직접 획득해서 들고 있음
         val connection = dataSource.connection
