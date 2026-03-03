cb5a5ea8e6a4d97b7dae7e18226050c8ccf000e5
jonghyunhub <chuwhdgus@gmail.com>
2026-03-03 20:05:32 +0900

    test : redis 테스트 설정 코드 추가

---
+++ Redis/src/test/kotlin/io/jonghyun/Redis/common/IntegrationTest.kt
@@ -0,0 +1,10 @@
+package io.jonghyun.Redis.common
+
+import org.junit.jupiter.api.Tag
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestConstructor
+
+@Tag("integration")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+abstract class IntegrationTest
